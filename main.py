e4b5cb9b0df880c8515bccbabd12dda1bef3a268
Rusheel Iyer <rusheel2911@gmail.com>
2022-01-21 11:56:20 +0100

    Add JSON handling and return prediction in JSON format

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 # [START app]
-from flask import Flask
+from flask import Flask, request, jsonify
 import pickle
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
 app = Flask(__name__)
 
-
 @app.route('/')
+def reroute():
+    return "Send a post request to the /predict endpoint"
+
+@app.route('/predict', methods=['POST'])
 def return_prediction():
+
+    request_data = request.get_json()
+
+    year = request_data['year']
+    month = request_data['month']
+    feature = str(year)+str(month)
+
     linear_model = pickle.load(open("linear_model.pkl", 'rb'))
-    message = str(linear_model.predict(np.asarray([202101]).reshape(1, -1))[0][0])
-    return message
+    prediction = {"prediction": linear_model.predict(np.asarray([feature]).reshape(1, -1))[0][0]}
+
+    return jsonify(prediction)
 
 
 @app.errorhandler(500)
 def server_error(e):
     # Log the error and stacktrace.
     return 'An internal error occurred.', 500
+
+if __name__ == '__main__':
+    # run app in debug mode on port 5000
+    app.run(debug=True, port=5000)
 # [END app]
